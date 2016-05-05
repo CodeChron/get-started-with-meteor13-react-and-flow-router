2885b2ae183ad560e5591cccc7136c27618e58d5
Anders Ramsay <andersr@gmail.com>
2016-05-05 19:52:20 -0400

    data mounting issue in progress

--- app/imports/ui/layouts/app_layout.jsx
+++ app/imports/ui/layouts/app_layout.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
 
 //Here we are passing in the props from our route into our main layout file.  The name 'content' here corresponds to the prop name in the route.  We need to 'call' the prop, ie using '()', because it's return value is a function, and this is what allows for passing through props and returning props via callbacks.
-export const App = ({content}) => <div>{content()}</div>
+
+export const AppLayout = (props) => {
+
+	console.log("content: ", props.foo)
+
+	// const test = props.foo
+	
+	return <div>{props.content()}</div>
+}
--- app/imports/ui/pages/homepage.jsx
+++ app/imports/ui/pages/homepage.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 //First, we pass in any props transferred to this component
 export const Homepage = (props) => {
 
+	console.log("homepage props: ", props.test)
+
   //Next, we create a function that accepts a userStatus boolean and return a message depending on if userStatus is true (signed in) or false (not signed in)
 	const displayLoginStatus = (userStatus) => {
+
 		const 
 		  isLoggedInMsg    = "You are signed in",
 		  isNotLoggedInMsg =  "You are not signed in"
@@ -16,5 +19,7 @@ export const Homepage = (props) => {
 	}
 
   //Finally, we call the function and pass in our reactive isLoggedIn data source, which we can access via props
-	return <div>{displayLoginStatus(props.isLoggedIn)}</div>
+	return <div>{displayLoginStatus(props.isLoggedIn)}
+	  <p>IsLoggedIn? {props.isLoggedIn}</p>
+	</div>
 }
